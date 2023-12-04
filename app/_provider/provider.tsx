f7f24c9c6f6c1ca6fb9c73142257bc26fd08cbec
Takkey-713 <takiyu713@gmail.com>
2023-12-04 22:41:10 +0900

    fix: providerの命名修正

--- app/_provider/provider.tsx
+++ app/_provider/provider.tsx
@@ -2,11 +2,13 @@
 
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
+// 以下にnext-authのプロバイダーを追加
+import theme from '../_styles/theme'
 
 export function Provider({ children }: { children: React.ReactNode }) {
   return (
     <CacheProvider>
-      <ChakraProvider>{children}</ChakraProvider>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
     </CacheProvider>
   )
 }

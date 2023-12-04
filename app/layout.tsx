e8ed16aba2f35a0ad5df139eac984491200cf7c6
Takkey-713 <takiyu713@gmail.com>
2023-12-04 21:41:51 +0900

    add: chakra-ui導入

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Provider } from './_providers/ChakraProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel='apple-touch-icon' href='/icon.png'></link>
         <meta name='theme-color' content='#f69435' />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Provider>{children}</Provider>
+      </body>
     </html>
   )
 }

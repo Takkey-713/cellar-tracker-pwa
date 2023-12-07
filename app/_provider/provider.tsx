fea81217a5401402f953609e6fd7c5221895e76a
Takkey-713 <takiyu713@gmail.com>
2023-12-07 19:03:25 +0900

    add: next-authのプロバイダーを追加

--- app/_provider/provider.tsx
+++ app/_provider/provider.tsx
@@ -1,13 +1,15 @@
 'use client'
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
-// 以下にnext-authのプロバイダーを追加
+import { SessionProvider } from 'next-auth/react'
 import theme from '../_styles/theme'
 
 export function Provider({ children }: { children: React.ReactNode }) {
   return (
     <CacheProvider>
-      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      <ChakraProvider theme={theme}>
+        <SessionProvider>{children}</SessionProvider>
+      </ChakraProvider>
     </CacheProvider>
   )
 }

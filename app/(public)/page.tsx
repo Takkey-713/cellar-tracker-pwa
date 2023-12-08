282c44d22e77edf9d4408d1a69d7ab2aababa8ab
Takkey-713 <takiyu713@gmail.com>
2023-12-08 16:12:59 +0900

    fix: トップページをpublic route groupに変更

---
+++ app/(public)/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { Flex } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Flex height='100vh' alignItems='center' justifyContent='center'>
+      <>
+        トップページです
+        {/* このページは基本的にどのユーザーもこれるページでマークアップは再度でOK
+          // 簡単な説明を入れるだけでいい ログイン OR サインアップの導線のみ設置する感じで */}
+      </>
+    </Flex>
+  )
+}

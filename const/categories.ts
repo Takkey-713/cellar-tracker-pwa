c2896bb699ee0772ab9667c7e47ee15f5c1c0a4a
Takkey-713 <takiyu713@gmail.com>
2023-12-18 22:41:17 +0900

    fix: カテゴリと品種が反対だったので修正

--- const/categories.ts
+++ const/categories.ts
@@ -1,24 +1,8 @@
 import { FormItem } from './types'
 
 export const categories: FormItem = [
-  { id: 1, name: 'ピノ・ノワール' },
-  { id: 2, name: 'シャルドネ' },
-  { id: 3, name: 'カベルネ・ソーヴィニヨン' },
-  { id: 4, name: 'メルロ' },
-  { id: 5, name: 'カベルネ・フラン' },
-  { id: 6, name: 'シラー' },
-  { id: 7, name: 'ピノ・ムニエ' },
-  { id: 8, name: 'ソーヴィニヨン・ブラン' },
-  { id: 9, name: 'グルナッシュ' },
-  { id: 10, name: 'プティ・ヴェルド' },
-  { id: 11, name: 'サンジョヴェーゼ' },
-  { id: 12, name: 'リースリング' },
-  { id: 13, name: 'ヴィオニエ' },
-  { id: 14, name: 'ゲヴェルツトラミネール' },
-  { id: 15, name: 'カベルネ・フラン' },
-  { id: 16, name: 'シュナン・ブラン' },
-  { id: 17, name: 'サンジョヴェーゼ' },
-  { id: 18, name: 'ネッビオーロ' },
-  { id: 19, name: 'モスカート' },
-  { id: 20, name: 'ピノ・ブラン' },
+  { id: 1, name: '赤ワイン' },
+  { id: 2, name: '白ワイン' },
+  { id: 3, name: 'ロゼワイン' },
+  { id: 4, name: 'スパークリングワイン' },
 ]
